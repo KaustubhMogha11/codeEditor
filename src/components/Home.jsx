780956982c96be5f1b012a436584d86a5eabf149
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-02-08 23:19:32 -0800

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+
+//components
+import Code from './Code';
+import Result from './Result';
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            <Result />
+            <footer id="main-footer">
+        Made by KM &copy; 2021
+    </footer>
+    
+        </>
+    )
+}
+
+export default Home;
